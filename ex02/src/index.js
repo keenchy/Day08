86df6e2ed73217ed189ea964645678b4a734816a
keenchy <h.kenan81@hotmail.com>
2021-03-15 21:33:21 +0100

    Functions with Arguments

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+// Wrote code below this line
+
+function functionPrintInfo() {
+    console.log("John", 23);
+    console.log("Michael", 43);
+
+}
+functionPrintInfo("John", 23);
+("Michael", 43);
+
+// Write code above this line
+
+module.exports = functionPrintInfo;
