edb0056ac33388c626eadbe09dee85f4f20cbe27
keenchy <h.kenan81@hotmail.com>
2021-03-15 22:37:18 +0100

    Functions Global vs. Scope

---
+++ ex05/src/index.js
@@ -0,0 +1,21 @@
+// Only change code below this line
+
+var dress = "ball qown";
+
+function myDress() {
+    var myDress = "little dress";
+
+
+    // Only change code above this line
+    return dress;
+}
+
+// Only change code below this line
+
+
+// Only change code above this line
+
+console.log(dress)
+
+
+module.exports = myDress;
